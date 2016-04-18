1669ded9eb8d16822356ec741947698df0c069a7
Siarhei Rabchykau <Siarhei_Rabchykau@epam.com>
2016-04-19 01:54:17 +0300

    Add new functional

--- src/by/epam/secondLesson/FindSecondLengthString.java
+++ src/by/epam/secondLesson/FindSecondLengthString.java
@@ -7,6 +7,15 @@ package by.epam.secondLesson;
 1) Добавь метод, который принимает массив и возвращает отсортированный по убыванию длин строк   - выполнено
 2) В отсортированном массиве верни второй элемент                                               - выполнено
 3) Предоставь возможность вводить размер массива и заполнять его через консоль                  - выполнено
+
+Я объеденил данный класс с ReplaceSymbols. Это ни на что не повлияет, но мне меньше писать)
+Сhange by places first and last letters in each second string of list or array
+    (Поменять местами первую и последнюю буквы в каждой второй строке списка).
+
+1) Предоставь возможность вводить размер массива и заполнять его через консоль  -
+2) В задании написано, что в каждой второй строке списка нужно                  -
+    менять местами буквы. То есть, во второй, четвёртой и т.д.                  - В каждой второй и меняет (бага небыло)
+3) Меняй местами буквы в отдельном методе.                                      -
  */
 
 import java.util.Scanner;
@@ -26,22 +35,42 @@ public class FindSecondLengthString {
         if (choose.equals("y") || choose.equals("yes") || choose.equals("Yes")) {
             //this is false generate array
             //but this array show work of sort method
-            String[] words = {"2", "1234", "123",
-                    "1", "123456", "12345678",
-                    "123456789", "1234", "1234"};
+            //generateStrArrayRandLength();
+            String[] words = {"v", "123456", "qwerty", "testing", "wat", "soft", "rare"};
+
+            //print generated array
             printStrArray(words);
 
             //use one of find of second length element method
             //findSecondLength(words);
+
+            //Сhange by places first and last letters in each second string
+            replaceSymbols(words);
+
+            //print array with replaces symbols
+            printStrArray(words);
+
+            //sort array and print second element
             sortStrArray(words);
         } else {
             System.out.println("Enter array size ");
             arraySize = sc.nextInt();
             words = new String[arraySize];
+
+            //manual fill array
             enterArray(words);
+
+            //print filled array
             printStrArray(words);
+
+            //Сhange by places first and last letters in each second string
+            replaceSymbols(words);
+
+            //print array with replaces symbols
+            printStrArray(words);
+
+            //sort array and print second element
             sortStrArray(words);
-            printStrArray(words);
         }
         sc.close();
    }
@@ -104,14 +133,30 @@ public class FindSecondLengthString {
     }
 
     //this method doesn't show as well work of sorting method
-/*    //generate array with rand(0-10) length and fill rand number (0-100)
-    protected static String[] generateStrArrayRandLength() {
+    //generate array with rand(0-10) length and fill rand number (0-100)
+    private static void generateStrArrayRandLength() {
         int arraySize = (int) (Math.random() * 10);
         words = new String[arraySize];
         for (int i = 0; i < arraySize; i++) {
             words [i] = String.valueOf((int)(Math.random() * 10000));
             System.out.print(words [i] + "  ");
         }
-        return words;
-    }*/
+    }
+
+    //Сhange by places first and last letters in each second string
+    private static void replaceSymbols (String[] strArray) {
+        System.out.println("replaceSymbols method work");
+        for (int i = 0; i < strArray.length; i++) {
+            if (i % 2 == 0) {
+                char charWord[] = strArray[i].toCharArray();
+                int charLength  = charWord.length - 1;
+                char temp       = charWord[0];
+
+                charWord[0] = charWord[charLength];
+                charWord[charLength] = temp;
+
+                strArray[i] = new String (charWord);
+            }
+        }
+    }
 }
