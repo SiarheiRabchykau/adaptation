3ce28de1475a635a8462d9aa0873418664bf3d54
Siarhei Rabchykau <Siarhei_Rabchykau@epam.com>
2016-04-18 15:17:36 +0300

    Added manually enter matrix size and manual fill matrix. Added "best practices"

--- src/by/epam/secondLesson/DegreeRandomMatrix.java
+++ src/by/epam/secondLesson/DegreeRandomMatrix.java
@@ -3,48 +3,45 @@ package by.epam.secondLesson;
 /*  Turn a matrix on 90 degrees clockwise
     (Повернуть матрицу на 90 градусов по часовой стрелке)*/
 
+/*Issue
+1) Добавь в программу, чтобы был выбор: вводить размер матрицы и заполнять её через консоль или вводить размер матрицы и заполнять её случайными числами
+2) Ввод матрицы, заполнение матрицы, разворот матрицы - в отдельные методы
+ */
+
+import java.util.Scanner;
+
 public class DegreeRandomMatrix {
+    private static int[][] matrix;
+
     public static void main(String[] args) {
-        int matrixSize = 4;
-/*int matrixLenght = 0;
-int matrixHeight = 0;
-Scanner scanner = new Scanner(System.in);
-System.out.println("Enter matrix length "); 
-matrixLenght = scanner.nextInt;
-System.out.ptintln(" Enter matrix height ");
-matrixHeight = scanner.nextInt;
-int[][] = new int[matrixLenght][matrixHeight];
 
-I'm to lazy to change generate loop*/
-        int[][] matrix = new int[matrixSize][matrixSize];
+        int matrixSize;
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                matrix[i][j] = (int) (Math.random() * 10);
-            }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter matrix size ");
+        matrixSize = sc.nextInt();
+        System.out.println("Auto-generate matrix? (Y/n)");
+        String choose = sc.next();
+
+        if (choose.equals("y") || choose.equals("yes") || choose.equals("Yes")) {
+            generateMatrix(matrixSize);
+        } else {
+
+            enterMatrix(matrixSize);
+
         }
 
-        PrintMatrix(matrix);
+        printMatrix(matrix);
 
-        // rotate matrix
-        for (int k = 0; k < matrixSize / 2; k++) {
-            for (int j = k; j < matrixSize - 1 - k; j++) {
-                int tmp = matrix[j][k];
+        rotateMatrix(matrixSize);
 
-                matrix[j][k] = matrix[matrixSize - 1 - k][j];
-                matrix[matrixSize - 1 - k][j] = matrix[matrixSize - 1 - j][matrixSize - 1 - k];
-                matrix[matrixSize - 1 - j][matrixSize - 1 - k] = matrix[k][matrixSize - 1 - j];
-                matrix[k][matrixSize - 1 - j] = tmp;
-            }
-        }
+        printMatrix(matrix);
 
-        PrintMatrix(matrix);
-
-
+        sc.close();
     }
 
     //print matrix
-    private static void PrintMatrix(int m[][]) {
+    private static void printMatrix(int m[][]) {
         int size = m.length;
         for (int i = 0; i < size; i++) {
 
@@ -56,5 +53,43 @@ I'm to lazy to change generate loop*/
         System.out.print("\n");
     }
 
+    //manual enter matrix
+    private static void enterMatrix(int size ) {
+        System.out.println("After every number press \"Enter\", please");
+        System.out.println("Enter matrix: ");
+        Scanner sc = new Scanner(System.in);
 
+        matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.print("\n");
+    }
+
+    //random generate matrix
+    private static void generateMatrix(int matrixSize) {
+        matrix = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                matrix[i][j] = (int) (Math.random() * 10);
+            }
+        }
+    }
+
+    //rotate matrix
+    private static void rotateMatrix (int matrixSize) {
+        for (int k = 0; k < matrixSize / 2; k++) {
+            for (int j = k; j < matrixSize - 1 - k; j++) {
+                int tmp = matrix[j][k];
+
+                matrix[j][k] = matrix[matrixSize - 1 - k][j];
+                matrix[matrixSize - 1 - k][j] = matrix[matrixSize - 1 - j][matrixSize - 1 - k];
+                matrix[matrixSize - 1 - j][matrixSize - 1 - k] = matrix[k][matrixSize - 1 - j];
+                matrix[k][matrixSize - 1 - j] = tmp;
+            }
+        }
+    }
 }
