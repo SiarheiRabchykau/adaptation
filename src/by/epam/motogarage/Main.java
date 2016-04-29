59b8470021f3f8165cfe2a0ed1a865ec54b342e4
Siarhei Rabchykau <Siarhei_Rabchykau@epam.com>
2016-04-29 16:01:05 +0300

    Add posibility to read from file

--- src/by/epam/motogarage/Main.java
+++ src/by/epam/motogarage/Main.java
@@ -3,7 +3,7 @@ package by.epam.motogarage;
 /*Issue
  1) Каждый метод должен выполнять одно действие. В методе findPower и ввод данных, и поиск, такого быть не должно.
 
- 2) Вынеси компаратор в отдельный класс в таске по ООП
+ 2) Вынеси компаратор в отдельный класс в таске по ООП      -
 */
 
 import by.epam.motogarage.mototechnictype.Mototechnics;
@@ -11,7 +11,6 @@ import by.epam.motogarage.mototechnictype.motorcycle.ATV;
 import by.epam.motogarage.mototechnictype.motorcycle.SportBikes;
 import by.epam.motogarage.mototechnictype.motorcycle.TouristBike;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -19,17 +18,18 @@ public abstract class Main {
     private static int maxPower;
     private static int minPower;
 
+
     public static void main(String[] args) throws IOException {
 
         ArrayList<Mototechnics> motoList = new ArrayList<>();
 
-        SportBikes drozd = new SportBikes("Honda", "CBR1100xx", 300, 259, 163, 7500.50);
-        drozd.setCost(8000);
-        drozd.setIsNaked(true);
-        drozd.reduceWeight();
-        drozd.increasePower();
-        drozd.doWheelie();
-        drozd.makeSameNoise();
+        SportBikes blackbird = new SportBikes("Honda", "CBR1100xx", 300, 259, 163, 7500.50);
+        blackbird.setCost(8000);
+        blackbird.setIsNaked(true);
+        blackbird.reduceWeight();
+        blackbird.increasePower();
+        blackbird.doWheelie();
+        blackbird.makeSameNoise();
 
         TouristBike european = new TouristBike("Honda", "Pan European", 200, 350, 140, 2, 40, 3000);
         european.setIsNaked(false);
@@ -38,28 +38,28 @@ public abstract class Main {
         SportBikes cbr1000rr = new SportBikes("Honda", "CBR1000rr", 299, 200, 170, 8000);
         ATV Grizzly700 = new ATV("Yamaha", "Grizzly 700", 120, 400, 70, 15, 10000);
 
-        motoList.add(drozd);
+        motoList.add(blackbird);
         motoList.add(cbr1000rr);
         motoList.add(Grizzly700);
         motoList.add(european);
 
-        printArrayList(motoList);
+        ArrayList<Mototechnics> newMotoList = new ReadFile().readFileWithMoto(motoList);
 
-        calculateCostGarage(motoList);
+        printArrayList(newMotoList);
 
-        //sort by weight
-        Collections.sort(motoList, new CustomComparatorArrayList());
+        calculateCostGarage(newMotoList);
 
-        System.out.println("\nSort by weight");
+        System.out.println("\nSort by power");
+        //sort by power
+        Collections.sort(newMotoList, new CustomComparatorArrayList());
 
-        printArrayList(motoList);
+        printArrayList(newMotoList);
 
         enterPower();
 
-        findPower(motoList);
-
-        readData();
+        findPower(newMotoList);
     }
+
 
     private static void printArrayList(ArrayList<Mototechnics> list) {
         for (Mototechnics m : list) {
@@ -89,24 +89,12 @@ public abstract class Main {
     }
 
     private static void findPower(ArrayList<Mototechnics> list) {
-
+        System.out.println(list.size());
         for (Mototechnics m : list) {
             if (m.getPower() >= minPower && m.getPower() <= maxPower) {
                 System.out.printf("Power of %s: %s", m.getModel(), m.getPower());
                 System.out.println();
-            } else {
-                System.out.println("Your power data is incorrect! Bye!");
-                break;
             }
         }
-    }
-
-    private static void readData() throws IOException {
-        try {
-            File file = new File("C:\\Test\\Test.txt");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
     }
 }
